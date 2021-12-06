c38fccbfe539e765af5a4c407fa3f7a4f125ecc0
MirAbbasHussain <mirabbashussain786@gmail.com>
2021-12-06 09:49:55 +0530

    Added payment service

---
+++ payment-service/src/test/java/com/abbas/casestudy/payment/api/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abbas.casestudy.payment.api;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
